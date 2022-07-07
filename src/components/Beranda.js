567cf45f08481d7a6f7f597fa8f9f9d55b754ea9
salwaahmadz <salwaahmadz888@gmail.com>
2022-07-07 16:29:04 +0700

    Landing Page

---
+++ src/components/Beranda.js
@@ -0,0 +1,51 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { useState, useEffect } from "react"
+
+const Beranda = () => {
+
+    const [responsive, setResponsive] = useState(typeof window !== "undefined" ? window.innerWidth <= 768 : false)
+    useEffect(() => {
+      const Handle = () => {
+        if(window.innerWidth <= 768){
+            setResponsive(true)
+        }else{
+            setResponsive(false)
+        }
+      }
+        if(window !== "undefined") window.addEventListener("resize", Handle)
+    }, [])
+    
+    useEffect(() => {
+      
+    }, [responsive])
+
+    return(
+        <div className="beranda">
+            <Container className="text-white d-flex justify-content-start align-items-center" style={{marginTop: "160px"}}>
+
+                {responsive === false?
+                <>
+                <Row>
+                    <Col>
+                    <div className="title1">THE STYLE IS</div>
+                    <div className="title2">DEFINED BY YOURS</div>
+                    <hr style={{height:"5px", width:"45%"}}></hr>
+                    </Col>
+                </Row>
+                </>
+            :
+            <Row>
+                    <Col>
+                    <div className="title3">THE STYLE IS</div>
+                    <div className="title4">DEFINED BY YOURS</div>
+                    <hr style={{height:"5px", width:"45%"}}></hr>
+                    </Col>
+                </Row>
+                }
+
+            </Container>
+        </div>
+    )
+}
+
+export default Beranda
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,83 @@
+import {Navbar, Container, Nav} from "react-bootstrap"
+import logo from "../assets/logo/logo.png"
+import keranjangIco from "../assets/icon/cart.png"
+import profileIco from "../assets/icon/profile.png"
+import { useState, useEffect } from "react"
+
+const NavigationBar = () => {
+
+    const [responsive, setResponsive] = useState(typeof window !== "undefined" ? window.innerWidth <= 768 : false)
+    useEffect(() => {
+      const Handle = () => {
+        if(window.innerWidth <= 768){
+            setResponsive(true)
+        }else{
+            setResponsive(false)
+        }
+      }
+        if(window !== "undefined") window.addEventListener("resize", Handle)
+    }, [])
+    
+    useEffect(() => {
+      
+    }, [responsive])
+
+    return(
+        <Navbar collapseOnSelect expand="md">
+            <Container>
+            <Navbar.Brand className="ms-3" href="/">
+            <img
+              alt="logo"
+              src={logo}
+              width={"128px"}
+              height={"64px"}
+              className="d-inline-block align-top"
+            />{' '}
+          </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+
+                {responsive === false?
+                <>
+                <div className="d-flex m-auto justify-content-center">
+                <Nav.Link className="navFont" style={{color: "#E53D00", fontWeight:"700"}}>Beranda</Nav.Link>
+                <Nav.Link className="navFont text-white">Katalog</Nav.Link>
+                <Nav.Link className="navFont text-white">Toko Offline</Nav.Link>
+                <Nav.Link className="navFont text-white">Tentang Kami</Nav.Link>
+                </div>
+
+                <div className="d-flex justify-content-center ms-auto">
+                    <Nav.Link>
+                    <img
+                    alt=""
+                    src={keranjangIco}
+                    className="d-inline-block align-top"
+                    />{' '}
+                    </Nav.Link>
+
+                    <Nav.Link>
+                    <img
+                    alt=""
+                    src={profileIco}
+                    className="d-inline-block align-top"
+                    />{' '}
+                    </Nav.Link>    
+                </div>
+                </>
+            :
+                <div>
+                    <Nav.Link className="navFont" style={{color: "#E53D00", fontWeight:"700"}}>Beranda</Nav.Link>
+                    <Nav.Link className="navFont text-white">Katalog</Nav.Link>
+                    <Nav.Link className="navFont text-white">Toko Offline</Nav.Link>
+                    <Nav.Link className="navFont text-white">Tentang Kami</Nav.Link>
+                    <Nav.Link className="navFont text-white">Keranjang</Nav.Link>
+                    <Nav.Link className="navFont text-white">Profile</Nav.Link>
+                </div>
+            }
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default NavigationBar
